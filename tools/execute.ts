c8904974e3e67de70b3883be5dda87a767f44dac
Rick Waterman <rick.waterman@jupiterone.com>
2021-10-29 11:35:46 -0700

    chore: remove console.log

--- tools/execute.ts
+++ tools/execute.ts
@@ -2,11 +2,6 @@
 import { executeIntegrationLocal } from "@jupiterone/jupiter-managed-integration-sdk";
 import invocationConfig from "../src/index";
 
-console.log({
-  hackeroneApiKey: process.env.HACKERONE_API_KEY,
-  hackeroneApiKeyName: process.env.HACKERONE_API_KEY_NAME,
-  hackeroneProgramHandle: process.env.HACKERONE_PROGRAM_HANDLE,
-});
 const integrationConfig = {
   hackeroneApiKey: process.env.HACKERONE_API_KEY,
   hackeroneApiKeyName: process.env.HACKERONE_API_KEY_NAME,
